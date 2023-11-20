17850c6920050f97846dfa3757f7f56a8768c6fa
ethanwaggoner <ethanjwaggoner@gmail.com>
2023-11-20 02:24:19 -0500

    quick code cleanup of token views

--- backend/api/views/token.py
+++ backend/api/views/token.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import logout
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.tokens import UntypedToken
@@ -8,18 +8,18 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
+def set_secure_cookie(response, key, value):
+    response.set_cookie(
+        key=key, value=value, httponly=True, samesite='None', secure=True
+    )
+
+
 class CustomTokenObtainPairView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         if response.status_code == status.HTTP_200_OK:
-            access_token = response.data['access']
-            refresh_token = response.data['refresh']
-            response.set_cookie(
-                key='access_token', value=access_token, httponly=True, samesite='None', secure=True
-            )
-            response.set_cookie(
-                key='refresh_token', value=refresh_token, httponly=True, samesite='None', secure=True
-            )
+            set_secure_cookie(response, 'access_token', response.data['access'])
+            set_secure_cookie(response, 'refresh_token', response.data['refresh'])
             response.data = {'message': 'Authentication successful'}
         return response
 
@@ -29,29 +29,22 @@ class CustomTokenRefreshView(TokenRefreshView):
         request.data['refresh'] = request.COOKIES.get('refresh_token')
         response = super().post(request, *args, **kwargs)
         if response.status_code == status.HTTP_200_OK:
-            access_token = response.data['access']
-            response.set_cookie(
-                key='access_token', value=access_token, httponly=True, samesite='None', secure=True
-            )
+            set_secure_cookie(response, 'access_token', response.data['access'])
             response.data = {'message': 'Token refreshed successfully'}
         return response
 
 
 @api_view(['GET'])
 def verify_token(request):
+    token = request.COOKIES.get('access_token')
+    if not token:
+        return Response({'detail': 'No token provided.'}, status=status.HTTP_401_UNAUTHORIZED)
+
     try:
-        token = request.COOKIES.get('access_token')
-        if not token:
-            return Response({'detail': 'No token provided.'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        try:
-            UntypedToken(token)
-        except (InvalidToken, TokenError) as e:
-            return Response({'detail': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
-
+        UntypedToken(token)
         return Response({'message': 'Token is valid'}, status=status.HTTP_200_OK)
-    except Exception as e:
-        return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    except (InvalidToken, TokenError) as e:
+        return Response({'detail': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 @api_view(['POST'])
@@ -60,5 +53,4 @@ def logout_user(request):
     response.delete_cookie('access_token')
     response.delete_cookie('refresh_token')
     logout(request)
-
     return response
