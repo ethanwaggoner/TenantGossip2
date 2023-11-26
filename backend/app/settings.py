f3dd0c63d8d292b562b8cf52817964cb4f6b0cb2
ethanwaggoner <ethanjwaggoner@gmail.com>
2023-11-26 13:03:08 -0500

    change logger level

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -186,7 +186,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['file'],
-            'level': 'DEBUG',
+            'level': 'INFO',
             'propagate': True,
         },
     },
